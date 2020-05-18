325cccb40e68b62b65cda57f6aece3004c62e264
Byambatsogt Enkhbat <enkhbat@Byambatsogts-MacBook-Pro.local>
2020-05-18 14:35:07 -0500

    added new csv

--- explore.py
+++ explore.py
@@ -1,22 +1,80 @@
 import pandas as pd
+import json
+import tweepy
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy import Cursor
+from datetime import datetime, date, time, timedelta
+from collections import Counter
+import sys
+# Load credentials from json file
+with open("../twitter_credentials.json", "r") as file:
+    creds = json.load(file)
 
-df=pd.read_csv("data/20200515_183816_byamba_tweets.csv")
+auth = tweepy.OAuthHandler(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
+auth.set_access_token(creds['ACCESS_TOKEN'], creds['ACCESS_SECRET'])
+api = tweepy.API(auth, wait_on_rate_limit=True)
+# df=pd.read_csv("data/20200515_183816_byamba_tweets.csv")
 
-#------------ What tweeets go viral today
-rt=df.drop_duplicates(subset=['text'])
-rt=df.sort_values(by=['retweetcount'], ascending=False)
+# #------------ What tweeets go viral today
+# rt=df.drop_duplicates(subset=['text'])
+# rt=df.sort_values(by=['retweetcount'], ascending=False)
 
 
-#-----------Who has the most followers-----
+# #-----------Who has the most followers-----
 
-follower=df.drop_duplicates(subset='username')
-fl=follower.sort_values(by=['followers'], ascending=False)
-fl=fl[['username', 'following', 'followers', 'totaltweets']]
-fl.to_csv('sorted_by_follwers.csv')
+# follower=df.drop_duplicates(subset='username')
+# fl=follower.sort_values(by=['followers'], ascending=False)
+# fl=fl[['username', 'following', 'followers', 'totaltweets']]
+# fl.to_csv('sorted_by_follwers.csv')
 
-#-----------Who tweets most-----
+# #-----------Who tweets most-----
 
-tws=df.drop_duplicates(subset='username')
-tws=tws.sort_values(by=['totaltweets'], ascending=False)
-tws=tws[['username', 'following', 'followers', 'totaltweets']]
-tws.to_csv("sorted_by_tweets.csv", index=False)
+# tws=df.drop_duplicates(subset='username')
+# tws=tws.sort_values(by=['totaltweets'], ascending=False)
+# tws=tws[['username', 'following', 'followers', 'totaltweets']]
+# tws.to_csv("sorted_by_tweets.csv", index=False)
+
+df=pd.read_csv('canditates.csv')
+account_list=list(df.screen_name)
+def desc_users():
+	users={}
+	users["name"]=[]
+	users["screen_name"]=[]
+	users["description"]=[]
+	users["statuses_count"]=[]
+	users["friends_count"]=[]
+	users["followers_count"]=[]
+	users["account_age"]=[]
+	users["average_tweets_per_day"]=[]
+	if len(account_list) > 0:
+		for target in account_list:
+			try:
+				item = api.get_user(target)
+				users["name"].append(item.name)
+				users["screen_name"].append(item.screen_name)
+				users["description"].append(item.description)
+				users["statuses_count"].append(item.statuses_count)
+				users["friends_count"].append(item.friends_count)
+				users["followers_count"].append(item.followers_count)
+				tweets = item.statuses_count
+				account_created_date = item.created_at
+				delta = datetime.utcnow() - account_created_date
+				account_age_days = delta.days
+				users["account_age"].append(account_age_days)
+				print("Account age (in days): " + str(account_age_days))
+				if account_age_days > 0:
+					print("Average tweets per day: " + "%.2f"%(float(tweets)/float(account_age_days)))
+					users["average_tweets_per_day"].append(float(tweets)/float(account_age_days))
+			except:
+				print("not found:{}".format(target))
+	df=pd.DataFrame(users, columns=users.keys())
+	return df.to_csv("can_stats.csv", index=False)
+
+desc_users()
+tws=pd.read_csv("can_stats.csv")
+tws=tws.sort_values(by=['average_tweets_per_day'], ascending=False)
+tws=tws[['name', 'average_tweets_per_day']]
+tws=tws.round(2)
+tws.to_csv("cand_sorted_by_tweets.csv", index=False)
+
---
+++ tweets.py
@@ -0,0 +1,59 @@
+#Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import tweepy
+import pandas as pd
+import numpy as np
+import regex as re
+import os
+import json
+from datetime import datetime, date, time, timedelta
+import time
+
+# Load credentials from json file
+with open("../twitter_credentials.json", "r") as file:
+	creds = json.load(file)
+
+auth = tweepy.OAuthHandler(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
+auth.set_access_token(creds['ACCESS_TOKEN'], creds['ACCESS_SECRET'])
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+# Define the search term and the date_since date as variables
+search_words = "covid"
+date_since = "2017-04-25"
+date_to="2020-05-15"
+
+def get_tweets_csv(search_words, date_since):
+	new_search = search_words + " -filter:retweets"
+
+	tweets = tweepy.Cursor(api.search, 
+	                           q="*",
+	                           lang="ru",
+                             geocode="47.9173,106.9177,10km",
+                             since=date_since,
+														 tweet_mode='extended').items(500)
+
+	users_locs = [[tweet.id_str, tweet.user.screen_name, tweet.full_text, tweet.created_at, tweet.user.location
+	              ,tweet.favorite_count,tweet.retweet_count] for tweet in tweets]
+	users_locs
+
+	tweet_text = pd.DataFrame(data=users_locs, 
+	                    columns=["id", "user", "tweets","date","location","favorites","retweets"])
+
+	return tweet_text
+
+df=get_tweets_csv(search_words, date_since)
+reduc=df.drop_duplicates(subset=['tweets'])
+part=reduc.sort_values(by=['retweets'], ascending=False)
+part=part[['tweets','retweets']]
+
+def get_user_name(x):
+	domain= x.split('@')[1]
+	name=domain.split(':')[0]
+	return name
+
+part=part[part.retweets>10]
+part["maker"]=part.tweets.map(lambda x: get_user_name(x))
+
+part.to_csv("top_tweets.csv")
