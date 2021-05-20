8e89db89ed2be5eb648005dafb38ae102d794ab4
Byambatsogt Enkhbat <enkhbat@Byambatsogts-MacBook-Pro.local>
2021-05-19 19:48:14 -0500

    added enkhbat.csv

--- tweets.py
+++ tweets.py
@@ -20,7 +20,7 @@ auth.set_access_token(creds['ACCESS_TOKEN'], creds['ACCESS_SECRET'])
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 # Define the search term and the date_since date as variables
-search_words = "covid"
+search_words = "ЗӨВ ХҮН"
 date_since = "2017-04-25"
 date_to="2020-05-15"
 
@@ -28,9 +28,9 @@ def get_tweets_csv(search_words, date_since):
 	new_search = search_words + " -filter:retweets"
 
 	tweets = tweepy.Cursor(api.search, 
-	                           q="*",
+	                           q=search_words,
 	                           lang="ru",
-                             geocode="47.9173,106.9177,10km",
+                             geocode="47.9173,106.9177,20km",
                              since=date_since,
 														 tweet_mode='extended').items(500)
 
@@ -47,13 +47,18 @@ df=get_tweets_csv(search_words, date_since)
 reduc=df.drop_duplicates(subset=['tweets'])
 part=reduc.sort_values(by=['retweets'], ascending=False)
 part=part[['tweets','retweets']]
+# part=part[part.retweets>10]
 
-def get_user_name(x):
-	domain= x.split('@')[1]
-	name=domain.split(':')[0]
-	return name
+# def get_user_name(x):
+# 	domain= x.split('@')[1]
+# 	name=domain.split(':')[0]
+# 	return name
 
-part=part[part.retweets>10]
-part["maker"]=part.tweets.map(lambda x: get_user_name(x))
 
-part.to_csv("top_tweets.csv")
+# part["maker"]=part.tweets.map(lambda x: get_user_name(x))
+# df.to_csv("zuvhun.csv", index=False)
+conn = sqlite3.connect('data/tweets.db')
+table_name="tweets"
+df.to_sql(table_name, conn, if_exists='append', index=False)
+# part.to_csv("zuvhun_tweets.csv")
+
